c4bf8e7d82c2ff47551a1d836a79f24f76e9b06a
bleyk <bleyk@DESKTOP-O2U72BH>
2023-02-20 20:05:41 +0500

    Добавьте файлы проекта.

---
+++ Tasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            line = Console.ReadLine();
+
+
+            Console.WriteLine(line.Length);
+
+
+            if ((line.Length % 2) == 0)
+            {
+                Console.WriteLine("Четное");
+
+                string str1 = line.Substring(0, line.Length / 2);
+                string str2 = line.Substring(line.Length / 2, line.Length / 2);
+
+
+                char[] charArray1 = str1.ToCharArray();
+                char[] charArray2 = str2.ToCharArray();
+                Array.Reverse(charArray1);
+                Array.Reverse(charArray2);
+
+
+                string s1 = new string(charArray1);
+                string s2 = new string(charArray2);
+                Console.WriteLine(s1 + s2);
+            }
+            else
+            {
+
+                Console.WriteLine("не Четное");
+
+                char[] charArray1 = line.ToCharArray();
+                Array.Reverse(charArray1);
+                string a1 = new string(charArray1);
+
+                Console.WriteLine(a1 + line);
+
+            }
+
+        }
+    }
+}
